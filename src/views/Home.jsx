3d6795f935e5997b839e4abd1b3d1996bf5457cf
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-26 18:38:29 -0500

    modified file

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,10 @@
 import About from "../components/About/About";
+import BmiCalc from "../components/BmiCalc";
 import ChooseUs from "../components/ChooseUs";
 import Featured from "../components/Featured";
 import Gallery from "../components/Gallery";
 import Hero from "../components/Hero";
+import Pricing from "../components/Pricing/Pricing";
 import Testimonials from "../components/Testimonials/Testimonials";
 import Trainers from "../components/Trainers/Trainers";
 
@@ -16,6 +18,8 @@ export default function Home() {
       <Trainers />
       <Testimonials />
       <Gallery />
+      <BmiCalc />
+      <Pricing />
     </>
   );
 }
