db35588476ab3fa9d031785bca83437020aa91f9
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-26 17:56:32 -0500

    created file

---
+++ src/components/Testimonials/TestimonialBoxes.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import TestimonialBox from "./TestimonialBox";
+
+export default function TestimonialBoxes() {
+  return (
+    <>
+      {/* <TestimonialBox
+        text=" “I've been a member of Gymate for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="John Wick"
+        job="Starbucks Employee"
+      /> */}
+      <TestimonialBox
+        text=" “I've been a member of Gymate for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="Harry Potter"
+        job="CEO of Hogwarts"
+      />
+      {/* <TestimonialBox
+        text=" “I've been a member of Gymate for the past 6 months and it has been an amazing experience. The trainers are knowledgeable and supportive, the
+                  equipment is top-notch, and the community of members is friendly and
+                  encouraging.”"
+        name="Bill Gates"
+        job="Founder of Microsoft"
+      /> */}
+    </>
+  );
+}
