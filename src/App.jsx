4caa26349d828a3029fb4ac2cf9eeabc3b2550fd
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-27 17:15:23 -0500

    modified file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
-import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 import About from "./views/About";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./views/Login";
+import Signup from "./views/SignUp";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <AuthContextProvider>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
             <Route path="about" element={<About />} />
           </Routes>
           <Footer />
