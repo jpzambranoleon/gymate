904c106b04435c1cede200fd5ea15516fe9e3f8e
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-27 21:52:03 -0500

    modified file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
 import About from "./views/About";
